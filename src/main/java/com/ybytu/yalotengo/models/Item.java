01630fa08c2fcb429fdb97cb86aaaa1dcb81c6e4
Bruna Sonda <bsonda@gmail.com>
2025-08-06 20:09:50 +0200

    refactor(item): rename condition column to itemCondition to avoid reserved keyword conflict and standardize variables

--- src/main/java/com/ybytu/yalotengo/models/Item.java
+++ src/main/java/com/ybytu/yalotengo/models/Item.java
@@ -5,11 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "items")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "items")
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,22 +18,31 @@ public class Item {
     @Column(name = "title", nullable = false, length = 200)
     private String title;
 
-    @Column(name = "description", nullable = false, length = 800)
+    @Column(name = "description", nullable = false, length = 500)
     private String description;
 
     @Column(name = "type", nullable = false, length = 100)
     private String type;
 
-    @Column(name = "condition", nullable = false, length = 50)
-    private String condition;
+    @Column(name = "item_condition", nullable = false, length = 50)
+    private String itemCondition;
 
     @Column(name = "img_url", nullable = false, length = 300)
-    private String img_url;
+    private String imgUrl;
 
-    @Column(name = "fruits_required", nullable = false, length = 50)
-    private Integer fruits_required;
+    @Column(name = "fruits_required", nullable = false)
+    private Integer fruitsRequired;
 
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
+
+    public Item(String title, String description, String type, String condition, String img_url, Integer fruits_required){
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.itemCondition = itemCondition;
+        this.imgUrl = imgUrl;
+        this.fruitsRequired = fruitsRequired;
+    }
 }

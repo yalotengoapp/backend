778c52e9f082fc84e31b61f649672acdcc4c3851
Bruna Sonda <bsonda@gmail.com>
2025-08-08 10:10:23 +0200

    fix: add @Service annotation in ItemService, add ADMIN role in enum Role

--- src/main/java/com/ybytu/yalotengo/models/User.java
+++ src/main/java/com/ybytu/yalotengo/models/User.java
@@ -5,10 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
-
-//import com.ybytu.yalotengo.models.Role;
-import javax.management.relation.Role;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/ybytu/yalotengo/services/ItemService.java
+++ src/main/java/com/ybytu/yalotengo/services/ItemService.java
@@ -1,7 +1,9 @@
 package com.ybytu.yalotengo.services;
 
 import com.ybytu.yalotengo.repositories.ItemRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemService {
     private final ItemRepository itemRepository;
 

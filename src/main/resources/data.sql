c80e61f353386c12c10f9cfe7747c32412d57a45
Bruna Sonda <bsonda@gmail.com>
2025-08-24 22:14:04 +0200

    feat(data.sql): build data.sql and add initial users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email, password, role, fruit) VALUES
+('Mufasa', 'mufasa@lionking.com', 'Forest#54321@', 'ADMIN', '3'),
+('Pumba', 'pumba@lionking.com', 'Forest#54321@', 'USER', '3'),
+('Zazu', 'zazuuu@lionking.com', 'Forest#54321@', 'USER', '3'),
+('Scar', 'scar@badass.com', 'Forest#54321@', 'USER', '3');

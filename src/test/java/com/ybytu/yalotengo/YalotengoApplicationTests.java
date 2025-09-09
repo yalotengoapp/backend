a1a437cf344efcc6d28928d1003b85faae44de66
Bruna Sonda <bsonda@gmail.com>
2025-09-09 13:06:00 +0200

    docker: add docker test and start actions environment

--- src/test/java/com/ybytu/yalotengo/YalotengoApplicationTests.java
+++ src/test/java/com/ybytu/yalotengo/YalotengoApplicationTests.java
@@ -1,8 +1,10 @@
 package com.ybytu.yalotengo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class YalotengoApplicationTests {
 
 	@Test
